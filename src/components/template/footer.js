079017c2a9238e9ab475c86085ef2693c9f754cf
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-04-08 23:51:18 +0700

    fix: footer text size responsive

--- src/components/template/footer.js
+++ src/components/template/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <div className="bg-gray-100">
+      <div className="bg-gray-100 sm:text-xs md:text-base">
         <div className="max-w-6xl mx-auto px-4 py-5">
           Copyright &copy; 2023 Samuel Christopher Santo - Harisenin FSD 4 Tugas
           React JS
